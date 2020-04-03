31e936b643081ae9550e9e62847d70f1ded92c86
Sofanshik <n0975426435@gmail.com>
2020-04-03 22:19:18 +0300

    Delete some rubbish

--- app/post/views.py
+++ app/post/views.py
@@ -6,6 +6,3 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'post/index.html', context={'posts': posts})
 
-#def post_details(request, slug):
-    #post_information = Post.objects.get(slug__iexact=slug)
-    #return render(request, 'blog/post_detail.html', context={'post_information': post_information})
