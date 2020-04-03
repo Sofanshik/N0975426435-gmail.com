e4bd6aaadc073d7b03ea1af704125d014ec403bc
Sofanshik <n0975426435@gmail.com>
2020-04-03 22:15:09 +0300

    Delete some rubbish

--- app/post/models.py
+++ app/post/models.py
@@ -4,23 +4,23 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     text = models.TextField(blank=True, db_index=True)
-    Date_publish = models.DateTimeField(auto_now_add=True)
+    date_publish = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{}'.format(self.title)
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30, db_index=True)
-    last_name = models.CharField(max_length=30, db_index=True)
+#class User(models.Model):
+    #first_name = models.CharField(max_length=30, db_index=True)
+    #last_name = models.CharField(max_length=30, db_index=True)
 
-    def __str__(self):
-        return '{}'.format(self.first_name)
+    #def __str__(self):
+       # return '{}'.format(self.first_name)
 
-class Topic(models.Model):
-    title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
-    value_of_post = models.IntegerField()
+#class Topic(models.Model):
+    #title = models.CharField(max_length=150, db_index=True)
+    #slug = models.SlugField(max_length=150, unique=True)
+    #value_of_post = models.IntegerField()
 
-    def __str__(self):
-        return '{}'.format(self.value_of_post)
+    #def __str__(self):
+        #return '{}'.format(self.value_of_post)
